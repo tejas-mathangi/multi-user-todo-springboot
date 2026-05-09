7d3d52249354f10fa6d8aa661c531d8c3a6d74fa
Tejas Mathangi <mathangi.tejas@gmail.com>
2026-05-09 16:57:03 +0530

    feat: add TodoController with basic endpoints

--- src/main/java/com/example/TodoApp/controller/TodoController.java
+++ src/main/java/com/example/TodoApp/controller/TodoController.java
@@ -1,4 +1,28 @@
 package com.example.TodoApp.controller;
 
+import com.example.TodoApp.model.Todo;
+import com.example.TodoApp.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
 public class TodoController {
+
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<Todo> getTodos() {
+        return todoService.getAllTodos();
+    }
+
+    @PostMapping
+    public Todo createTodo(@RequestBody Todo todo) {
+        return todoService.createTodo(todo);
+    }
 }
