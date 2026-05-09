de2cdda3f35339855cacd63cedecf2f0c36cdde8
Tejas Mathangi <mathangi.tejas@gmail.com>
2026-05-09 16:28:19 +0530

    feat: define Todo entity with fields and JPA annotations

--- src/main/java/com/example/TodoApp/model/Todo.java
+++ src/main/java/com/example/TodoApp/model/Todo.java
@@ -1,4 +1,17 @@
 package com.example.TodoApp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private boolean completed;
 }
