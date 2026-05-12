0dc91c8855743128c540fcdf0d6ede1c8f7bd1eb
Tejas Mathangi <mathangi.tejas@gmail.com>
2026-05-13 02:22:22 +0530

    fix: add Lombok annotations for JSON serialization

--- src/main/java/com/example/TodoApp/model/Todo.java
+++ src/main/java/com/example/TodoApp/model/Todo.java
@@ -1,9 +1,16 @@
 package com.example.TodoApp.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Todo {
 
     @Id
