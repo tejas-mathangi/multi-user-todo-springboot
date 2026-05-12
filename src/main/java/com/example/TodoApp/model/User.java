56e21daafccfaca2409fd187be2994bebebbfd27
Tejas Mathangi <mathangi.tejas@gmail.com>
2026-05-13 02:27:48 +0530

    feat: add User entity for multi-user support

---
+++ src/main/java/com/example/TodoApp/model/User.java
@@ -0,0 +1,21 @@
+package com.example.TodoApp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+}
